cde5ce9b2c4bfc9e6354cfbb762031e7f31c234f
Mohsen Hariri <mhariri68@gmail.com>
2024-07-31 14:21:44 -0400

    test profiling

--- llm/models/gpt/inference.py
+++ llm/models/gpt/inference.py
@@ -2,34 +2,42 @@ import tiktoken
 import torch
 from GPT2 import GPT2
 from GPT2Config import GPT2Config
+from helpers.profiling import experiment, profiler
 from init import init
 
-conf = init(seed=42)
 
-enc = tiktoken.get_encoding("gpt2")
+@experiment(experiment_name="gpt2", num_experiments=10, save_profile=True)
+def main():
+    conf = init(seed=42)
 
-batch_size = 5
-max_length = 30
+    enc = tiktoken.get_encoding("gpt2")
 
-prompt = "Hello, I'm a language model,"
-tokens = enc.encode(prompt)  # (sequence_length,)
-tokens = torch.tensor(tokens, dtype=torch.long)
-tokens = tokens.unsqueeze(0).repeat(batch_size, 1)  # (batch_size, sequence_length)
+    batch_size = 5
+    max_length = 30
 
-input_ids = tokens.to(conf.device)
+    prompt = "Hello, I'm a language model,"
+    tokens = enc.encode(prompt)  # (sequence_length,)
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0).repeat(batch_size, 1)  # (batch_size, sequence_length)
 
-config = GPT2Config()
-model = GPT2(config)
+    input_ids = tokens.to(conf.device)
 
-output_ids = model.generate(
-    input_ids=input_ids,
-    max_length=30,
-    num_return_sequences=1,
-    do_sample=True,
-    top_k=50,
-)
+    config = GPT2Config()
+    model = GPT2(config)
 
-for i in range(batch_size):
-    tokens = output_ids[i, :].tolist()
-    decoded = enc.decode(tokens)
-    print(">", decoded)
+    output_ids = model.generate(
+        input_ids=input_ids,
+        max_length=30,
+        num_return_sequences=1,
+        do_sample=True,
+        top_k=50,
+    )
+
+    for i in range(batch_size):
+        tokens = output_ids[i, :].tolist()
+        decoded = enc.decode(tokens)
+        print(">", decoded)
+
+
+if __name__ == "__main__":
+    main()
